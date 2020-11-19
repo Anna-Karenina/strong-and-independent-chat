3dca22f4914aad37d3e236b1a932e0362e66edcf
Vladimir <vladimir.suragin@gmail.com>
2020-11-19 22:58:54 +0300

    chats: options & modal

--- static/pages/chats/chats.js
+++ static/pages/chats/chats.js
@@ -5,3 +5,24 @@ const inputSetter = (input) => (event) => {
 };
 
 inputs.forEach((input) => input.addEventListener('input', inputSetter(input)));
+
+const modal = (selector) => {
+  const $modal = document.querySelector(selector);
+  if (!$modal) return { open: () => {} };
+
+  $modal.addEventListener('click', (e) => {
+    if (e.target === $modal) {
+      $modal.classList.remove('modal-show');
+    }
+  });
+
+  return {
+    open: () => $modal.classList.add('modal-show'),
+  };
+};
+
+const addUserAction = document.querySelector('.add-user-action');
+if (addUserAction) {
+  const addUserModal = modal('.add-user-modal');
+  addUserAction.addEventListener('click', addUserModal.open);
+}
