bf77e6c0e883afcd4cab084511acc878d708c418
Vladimir <vladimir.suragin@gmail.com>
2020-12-02 20:37:01 +0300

    chats use templator

--- static/chats/chats.js
+++ static/chats/chats.js
@@ -1,8 +1,21 @@
 
+import Chats from '/blocks/Chats/index.js';
+import { render } from '/core/templator/index.js'
 import { modal } from '/js/modal.js';
-import { onFormSubmit } from '/js/formSubmit.js';
 
-onFormSubmit('.send-message', console.log);
+const onSubmit = (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const aggregatedFormData = Object.fromEntries(formData.entries());
+  console.log(aggregatedFormData);
+};
+
+const onSearch = (e) => {
+  e.target.setAttribute('value', e.target.value);
+};
+
+const chats = new Chats({ onSubmit, onSearch });
+render('#app', chats);
   
 const addUserAction = document.querySelector('.add-user-action');
 if (addUserAction) {
