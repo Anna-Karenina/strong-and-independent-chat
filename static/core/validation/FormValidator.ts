015b92811e774ca86620fbcabd08c6ce3f59d2b2
Vladimir <vladimir.suragin@gmail.com>
2020-12-06 20:57:36 +0300

    form validator: validate all & valid/invalid getters

--- static/core/validation/FormValidator.ts
+++ static/core/validation/FormValidator.ts
@@ -4,7 +4,7 @@ interface IValidatorsMap {
   [fieldName: string]: ValidationScheme,
 };
 
-interface IFormState {
+export interface IFormState {
   [fieldName: string]: IValidatorResult,
 };
 
@@ -26,6 +26,14 @@ export default class FormValidator {
       }, {});
   }
 
+  get valid() {
+    return Object.values(this.formState).every((state) => state.valid);
+  }
+
+  get invalid() {
+    return !this.valid;
+  }
+
   validate(fieldName: string, value: string, ...additionalArgs: any[]) {
     const scheme = this.validatorsMap[fieldName];
     if (!scheme) {
@@ -34,5 +42,12 @@ export default class FormValidator {
 
     const result = scheme.validate(value, ...additionalArgs);
     this.formState = { ...this.formState, [fieldName]: result };
+  }
+
+  validateAll(fieldsMap: { [key: string]: any }, additionalArgsMap: { [key: string]: any[] } = {}) {
+    Object.entries(fieldsMap).forEach(([field, value]) => {
+      const additionalArgs = additionalArgsMap[field] || [];
+      this.validate(field, String(value), ...additionalArgs);
+    });
   }
 };
--- static/core/validation/index.ts
+++ static/core/validation/index.ts
@@ -1,3 +1,16 @@
 import ValidationScheme from './ValidationScheme.js';
+import FormValidator, { IFormState } from './FormValidator.js';
+import {
+  textFiledScheme,
+  emailScheme,
+  passwordDuplicateScheme,
+} from './schemes/schemes.js';
 
-export default ValidationScheme;
+export {
+  ValidationScheme,
+  FormValidator,
+  textFiledScheme,
+  emailScheme,
+  passwordDuplicateScheme,
+  IFormState,
+};
