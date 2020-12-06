9407ebf896f2a96112549cac24b96896c3280ad6
Vladimir <vladimir.suragin@gmail.com>
2020-12-06 17:39:58 +0300

    use static directory for source map

--- scripts/start.js
+++ scripts/start.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, '../', 'dist')));
+app.use('/static', express.static(path.join(__dirname, '../', 'static')));
 
 app.listen(PORT, function () {
   console.log(`Server started on port: ${PORT}`);
