704491690d1994138cabb580ba5f56b821ed1c27
Vladimir <vladimir.suragin@gmail.com>
2020-12-09 19:25:54 +0300

    404 & 500 pages: use ts

---
+++ static/blocks/500/Page500.ts
@@ -0,0 +1,21 @@
+import Component from '../../core/component/index.js';
+import Templator from '../../core/templator/index.js';
+import { template500 } from './500.template.js';
+
+interface IPage500Props {};
+
+export default class Page500 extends Component {
+  private templator: Templator;
+
+  constructor(props: IPage500Props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.templator = new Templator(template500);
+  }
+
+  render() {
+    return this.templator.render(this.props);
+  }
+};
---
+++ static/pages/404/404.ts
@@ -0,0 +1,5 @@
+import Page404 from '../../blocks/404/index.js';
+import { render } from '../../core/templator/index.js'
+
+const page404 = new Page404({});
+render('#app', page404);
