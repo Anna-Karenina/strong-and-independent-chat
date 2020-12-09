39ae3ef857f041fbee2410d7ece0dd55a084dfd6
Vladimir <vladimir.suragin@gmail.com>
2020-12-09 20:17:38 +0300

    cut util: remove args mutation

--- static/core/templator/utils/cut.ts
+++ static/core/templator/utils/cut.ts
@@ -20,13 +20,14 @@ const generateParsingTemplateError = (template: string): Error => {
 };
 
 export const cutElementNode = (template: string, components: { [key: string]: ComponentConstructor }): TCutElementNodeResult => {
-  const openTag = OPEN_TAG_REGEXP.exec(template);
+  let _template = template;
+  const openTag = OPEN_TAG_REGEXP.exec(_template);
   if (!openTag) {
-    throw generateParsingTemplateError(template);
+    throw generateParsingTemplateError(_template);
   }
   const [tag, tagName] = openTag;
   
-  template = template.slice(openTag.index + tag.length);
+  _template = _template.slice(openTag.index + tag.length);
   const beforeCloseTagRegExp = new RegExp(`(.*?)</${tagName}.*?>`);
   const openSimilarTagRegExp = new RegExp(`<${tagName}.*?>`, 'g');
   const closeSimilarTagRegExp = new RegExp(`</${tagName}.*?>`, 'g');
@@ -35,13 +36,13 @@ export const cutElementNode = (template: string, components: { [key: string]: Co
   let level = 1;
   let key = null;
 
-  while((key = beforeCloseTagRegExp.exec(template))) {
+  while((key = beforeCloseTagRegExp.exec(_template))) {
     const [matched, matchedWithoutCloseTag] = key;
 
     level += (matched.match(openSimilarTagRegExp) || []).length;
     level -= (matched.match(closeSimilarTagRegExp) || []).length;
 
-    template = template.slice(key.index + matched.length);
+    _template = _template.slice(key.index + matched.length);
 
     if (level <= 0) {
       tagContent += matchedWithoutCloseTag;
@@ -56,7 +57,7 @@ export const cutElementNode = (template: string, components: { [key: string]: Co
     : new VElementNode(tag);
 
   const element = { node, content: tagContent.trim() };
-  return [element, template];
+  return [element, _template];
 }
 
 export const cutTextNode = (template: string): TCutTextNodeResult => {
