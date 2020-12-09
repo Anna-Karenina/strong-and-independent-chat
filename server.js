56b3e6a391eaec9da9ca4cdb1b9a1053ab3dfda9
Vladimir <vladimir.suragin@gmail.com>
2020-12-09 21:40:17 +0300

    very big structure refactoring

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use('/static', express.static(path.join(__dirname, 'static')));
+app.use('/src', express.static(path.join(__dirname, 'src')));
 
 app.listen(PORT, function () {
   console.log(`Server started on port: ${PORT}`);
--- static/pages/500/500.ts
+++
@@ -1,5 +0,0 @@
-import Page500 from '../../blocks/500/index.js';
-import {render} from '../../core/templator/index.js'
-
-const page500 = new Page500({});
-render('#app', page500);
