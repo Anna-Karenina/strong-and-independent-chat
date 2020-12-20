abef36ff721bfb613c418d0d8f9a08517a4806d9
Vladimir <vladimir.suragin@gmail.com>
2020-12-20 12:40:58 +0300

    component v2

---
+++ src/core/componentV2/index.ts
@@ -0,0 +1,12 @@
+import Component, {IProps} from './Component.js';
+
+interface ComponentConstructor {
+  new (props: IProps): Component;
+};
+
+export default Component;
+
+export {
+  IProps,
+  ComponentConstructor,
+};
