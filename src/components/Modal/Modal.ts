a5a04f4772a801af738ef93aed82275ae7622dd8
Vladimir <vladimir.suragin@gmail.com>
2020-12-24 23:04:06 +0300

    modal component

---
+++ src/components/Modal/Modal.ts
@@ -0,0 +1,39 @@
+import Component from '../../core/component/index.js';
+import VNode from '../../core/templator/VNode/VNode.js';
+import Templator from '../../core/templator/index.js'
+import {modalTemplate} from './modal.template.js';
+
+interface IModalProps {
+  show: boolean,
+  onClose: Function,
+  $children: VNode[],
+};
+
+const templator = Templator.compile(modalTemplate);
+
+export default class MyButton extends Component<IModalProps> {
+  constructor(props: IModalProps) {
+    super(props);
+  }
+
+  onModalClick = (e: InputEvent) => {
+    if (this.element === e.target) {
+      this.props?.onClose();
+    }
+  }
+
+  render() {
+    const {show} = this.props;
+    const classes = ['modal'];
+
+    if (show) {
+      classes.push('modal-show');
+    }
+
+    return templator({
+      $children: this.props.$children,
+      className: classes.join(' '),
+      onModalClick: this.onModalClick,
+    });
+  }
+}
---
+++ src/components/Modal/modal.template.ts
@@ -0,0 +1,5 @@
+export const modalTemplate = `
+  <div :class="className" @click="onModalClick">
+    $children$
+  </div>
+`;
