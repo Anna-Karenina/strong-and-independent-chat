18289e0ef678321cabb0069356bf76a9df9aa5af
Vladimir <vladimir.suragin@gmail.com>
2021-01-08 11:06:31 +0300

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetWebpackPlugin(),
+      new TerserWebpackPlugin()
+    ]
+  }
+
+  return config
+};
+
+const cssLoaders = (...additive) => [
+  {
+    loader: MiniCssExtractPlugin.loader,
+  },
+  'css-loader',
+  ...additive,
+];
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: isDev ? 'development' : 'production',
+  entry: './main.ts',
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src', 'core'),
+      '@components': path.resolve(__dirname, 'src', 'components'),
+    }
+  },
+  optimization: optimization(),
+  devtool: isDev ? 'source-map' : '',
+  devServer: {
+    port: 4000,
+    hot: true,
+    // https: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+      minify: {
+        collapseWhitespace: isProd
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: cssLoaders()
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: cssLoaders('sass-loader')
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'tsconfig.json'),
+            },
+          },
+        ],
+        exclude: /(node_modules)/
+      }
+    ]
+  }
+};
