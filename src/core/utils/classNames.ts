11210f4fbfb03580f7004c57a90ee45eaa059952
Vladimir <vladimir.suragin@gmail.com>
2021-01-08 09:53:49 +0300

    classNames refactoring

--- src/core/utils/classNames.ts
+++ src/core/utils/classNames.ts
@@ -1,24 +1,27 @@
 import {isPlainObject} from './isPlainObject.js';
 import {identity} from './identity.js';
 
-const classReducer = (rawClass: unknown): string[] => {
-  if (typeof rawClass === 'string') return [rawClass.trim()];
+const classReducer = (rawClass: unknown): string => {
+  if (!rawClass) return '';
+
+  if (typeof rawClass === 'string') return rawClass.trim();
 
   if (isPlainObject(rawClass)) {
     return Object
       .entries(rawClass)
       .filter(([, isExist]) => !!isExist)
-      .map(([className]) => className.trim());
+      .map(([className]) => className.trim())
+      .join(' ');
   }
 
   if (Array.isArray(rawClass)) {
-    return rawClass.flatMap(classReducer);
+    return classNames.apply(null, rawClass);
   }
 
-  return [];
+  return '';
 };
 
 export const classNames = (...rawClasses: unknown[]) => rawClasses
-  .flatMap(classReducer)
+  .map(classReducer)
   .filter(identity)
   .join(' ');
