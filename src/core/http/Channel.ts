6b69b6184bb211c570706e672e4ca1e83cfd0d33
Vladimir <vladimir.suragin@gmail.com>
2021-01-09 13:59:52 +0300

    Channel

---
+++ src/core/http/Channel.ts
@@ -0,0 +1,61 @@
+import EventBus from '@core/bus';
+
+export default class Channel {
+  private ws: WebSocket;
+  private bus: () => EventBus;
+
+  constructor(url: string) {
+    this.ws = new WebSocket(url);
+
+    const bus = new EventBus();
+    this.bus = () => bus;
+
+    this.initListeners();
+  }
+
+  private initListeners() {
+    this.ws.addEventListener('open', this.onOpen);
+    this.ws.addEventListener('error', this.onError);
+    this.ws.addEventListener('close', this.onClose);
+    this.ws.addEventListener('message', this.onMessage);
+  }
+
+  private removeListeners() {
+    this.ws.removeEventListener('open', this.onOpen);
+    this.ws.removeEventListener('error', this.onError);
+    this.ws.removeEventListener('close', this.onClose);
+    this.ws.removeEventListener('message', this.onMessage);
+  }
+
+  private onOpen = () => {
+    console.log('[open] Connection is established');
+  }
+
+  private onError = (event: ErrorEvent) => {
+    console.error(`[error] ${event.message}`);
+  }
+
+  private onClose = (event: CloseEvent) => {
+    if (event.wasClean) {
+      console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    } else {
+      console.log('[close] Connection has been terminated');
+    }
+  }
+
+  private onMessage = (event: MessageEvent) => {
+    if (this.bus().hasEvent('message')) {
+      this.bus().emit('message', event.data);
+    }
+  }
+
+  subscribe(cb: (data: any) => void) {
+    this.bus().on('message', cb);
+  }
+
+  disconnect() {
+    this.removeListeners();
+    this.bus().offAll();
+    this.ws.close();
+  }
+}
--- src/core/http/index.ts
+++ src/core/http/index.ts
@@ -1,10 +1,12 @@
 import {METHODS, HOST} from './constants';
 import HTTPTransport from './HTTPTransport';
+import Channel from './Channel';
 
 const http = new HTTPTransport(`${HOST}/api/v2`);
 
 export {
   HTTPTransport,
+  Channel,
   HOST,
   METHODS,
   http,
